b15c233b84b9703fdbcaf3fec86042fa7bab8039
phbo85 <p.bochmann@googlemail.com>
2021-10-01 19:32:25 +0200

    add empty home

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,3 @@
-import fetcher from 'swr-eth';
-import { SWRConfig } from 'swr';
-import { useRouter } from 'next/router';
-
-import { useWeb3React } from '@web3-react/core';
-
-import Header from '../components/Header';
-import Bot from '../components/Bot';
-import useChainSettings from '../hooks/useChainSettings';
-
-import SwapABI from '../abi/swap.abi.json';
-import ERC20ABI from '../abi/ERC20.abi.json';
-
 export default function Home() {
-  const { active, library, chainId } = useWeb3React();
-  const { color, logo } = useChainSettings(chainId);
-
-  const { query } = useRouter();
-  const { c } = query;
-
-  // get contract for stable
-  return (
-    <>
-      <Header color={color} logo={logo} />
-      {active ? (
-        <SWRConfig value={{ fetcher: fetcher(library, [SwapABI, ERC20ABI]) }}>
-          <Bot smartContract={c} />
-        </SWRConfig>
-      ) : (
-        <div>Please connect your wallet</div>
-      )}
-    </>
-  );
+  return <div>empty</div>;
 }
