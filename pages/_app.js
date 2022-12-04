6906573eca3c7c962cfa1a945bfd0ab3d541a222
Philipp <p.bochmann@googlemail.com>
2022-12-04 18:32:41 +0100

    remove unused packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,12 @@
-import { Web3ReactProvider } from '@web3-react/core';
-import { Web3Provider } from '@ethersproject/providers';
 import { ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/montserrat';
 import '@fontsource/montserrat/900-italic.css';
 import theme from '../styles/theme';
 
-function getLibrary(provider) {
-  const library = new Web3Provider(provider);
-  library.pollingInterval = 12000;
-
-  return library;
-}
-
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <Component {...pageProps} />
-      </Web3ReactProvider>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
